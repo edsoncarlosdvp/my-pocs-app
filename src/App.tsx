fcc78d2bca415bd191818824b7f403208778dfc5
edsoncarlosdvp <edsoncarlos@ymail.com>
2022-05-09 17:15:57 -0300

    Organizando configurações

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { useDispatch, useSelector } from "react-redux"
-import './App.css'
-import { RootState } from "./store/Stock"
-import { decrement, increment } from "./store/Stock/Stock.store"
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './store/Stock';
+import { decrement, increment } from './store/Stock/Stock.store';
+import './styles/App.css';
 
-export const App = (): JSX.Element => {
+export function App(): JSX.Element {
+  const dispatch = useDispatch();
+  const stock = useSelector((state: RootState) => state.stock);
 
-    const dispatch = useDispatch()
-    const stock = useSelector((state: RootState) => state.stock)
-
-    return (
-        <div className="App">
-            <div className="App-header">
-                {stock.counter}
-                <button className="button" onClick={() => dispatch(increment())}>Increment</button>
-                <button className="button" onClick={() => dispatch(decrement())}>Decrement</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="App">
+      <div className="App-header">
+        {stock.counter}
+        <button className="button" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <button className="button" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
